9bf9af024db1994f1f92e5b59854b1c937aeb905
Md Emon <md.emon.au@gmail.com>
2021-10-03 14:18:58 +0600

    Register Confirmation Controller with simple view done

--- DataImporter/DataImporter.Web/Controllers/AccountController.cs
+++ DataImporter/DataImporter.Web/Controllers/AccountController.cs
@@ -47,6 +47,46 @@ namespace DataImporter.Web.Controllers
             _mailService = mailService;
         }
 
+        public async Task<IActionResult> RegisterConfirmation(RegisterConfirmationModel model, string email, string returnUrl = null)
+        {
+            if (email == null)
+            {
+                return RedirectToPage("/Index");
+            }
+
+            var user = await _userManager.FindByEmailAsync(email);
+            if (user == null)
+            {
+                return NotFound($"Unable to load user with email '{email}'.");
+            }
+
+            model.Email = email;
+
+            return View(model);
+        }
+
+        [TempData]
+        public string StatusMessage { get; set; }
+
+        public async Task<IActionResult> ConfirmEmail(string userId, string code)
+        {
+            if (userId == null || code == null)
+            {
+                return RedirectToPage("/Index");
+            }
+
+            var user = await _userManager.FindByIdAsync(userId);
+            if (user == null)
+            {
+                return NotFound($"Unable to load user with ID '{userId}'.");
+            }
+
+            code = Encoding.UTF8.GetString(WebEncoders.Base64UrlDecode(code));
+            var result = await _userManager.ConfirmEmailAsync(user, code);
+            StatusMessage = result.Succeeded ? "Thank you for confirming your email." : "Error confirming your email.";
+            return View();
+        }
+
         public async Task<IActionResult> Register(string returnUrl = null)
         {
             var model = new RegisterModel();
---
+++ DataImporter/DataImporter.Web/Models/Account/RegisterConfirmationModel.cs
@@ -0,0 +1,11 @@
+﻿namespace DataImporter.Web.Models.Account
+{
+    public class RegisterConfirmationModel
+    {
+        public string Email { get; set; }
+
+        public bool DisplayConfirmAccountLink { get; set; }
+
+        public string EmailConfirmationUrl { get; set; }
+    }
+}
