d8b6df38ef3a87801e202daa18fc1398233dcf88
Mr John <john@joh.com>
2021-10-24 22:48:44 +0600

    10 test done

--- DataImporter/Test/DataImporter.Importing.Test/DataImporter.Importing.Tests/GroupServiceTests.cs
+++ DataImporter/Test/DataImporter.Importing.Test/DataImporter.Importing.Tests/GroupServiceTests.cs
@@ -1,6 +1,7 @@
 using Autofac.Extras.Moq;
 using AutoMapper;
 using DataImporter.Importing.BusinessObjects;
+using DataImporter.Importing.Exceptions;
 using DataImporter.Importing.Repositories;
 using DataImporter.Importing.Services;
 using DataImporter.Importing.UnitOfWorks;
@@ -54,10 +55,156 @@ namespace DataImporter.Importing.Tests
         }
 
         [Test]
-        public void GetGroup_GroupExists_ReturnGroup()
+        public void CreateGroup_ValidGroup_CreteGroup()
         {
             // Arrange
-            var Id = 1; 
+            var group = new Group()
+            {
+                Name = "Books",
+                ApplicationUserId = new Guid("FA8E36E2-D9A4-4F41-CD41-08D98C6D37B8")
+            };
+
+            _importingUnitOfWork.Setup(x => x.Groups)
+                .Returns(_groupRepository.Object);
+
+            _groupRepository.Setup(x => x.GetById(0))
+                .Returns(new Entities.Group());
+
+            _groupRepository.Setup(x => x.GetCount(It.IsAny<Expression<Func<Entities.Group, bool>>>()))
+                .Returns(1);
+
+            _mapperMock.Setup(x => x.Map<Group>(new Entities.Group())).Returns(group);
+
+            _importingUnitOfWork.Setup(x => x.Groups.Add(
+                It.IsAny<Entities.Group>()
+                ))
+                .Verifiable();
+
+            var fakeGuid = "FA8E36E2-D9A4-4F41-CD41-08D98C6D37B8";
+
+            var user = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
+            {
+                new Claim(ClaimTypes.Name, "example name"),
+                new Claim(ClaimTypes.NameIdentifier, fakeGuid),
+                new Claim("custom-claim", "example claim value"),
+            }, "mock"));
+
+            var context = new DefaultHttpContext();
+            context.User = user;
+
+
+            _httpContextAccessorMock.Setup(_ => _.HttpContext).Returns(context);
+
+            _httpContextAccessorMock.Setup(x => x.HttpContext.User)
+                .Returns(user);
+
+            // Act 
+            _groupService.CreateGroup(group);
+
+            // Assert
+            this.ShouldSatisfyAllConditions(
+                () => _importingUnitOfWork.Verify()
+            );
+        }
+
+        [Test]
+        public void CreateGroup_NullGroup_InvalidParameterException()
+        {
+            // Arrange 
+            Group group = null;
+
+            // Act 
+            try
+            {
+                _groupService.CreateGroup(group);
+            }
+            catch { }
+
+            // Assert 
+            var exception = Should.Throw<InvalidParameterException>(() => _groupService.CreateGroup(group), "Group was not provided");
+        }
+
+        [Test]
+        public void CreateGroup_DuplicateName_DuplicateNameException()
+        {
+            // Arrange 
+            var group = new Group()
+            {
+                Name = "Students"
+            };
+
+            _importingUnitOfWork.Setup(x => x.Groups)
+                .Returns(_groupRepository.Object);
+
+            _groupRepository.Setup(x => x.GetById(0))
+                .Returns(new Entities.Group());
+
+            _groupRepository.Setup(x => x.GetCount(It.IsAny<Expression<Func<Entities.Group, bool>>>()))
+                .Returns(1);
+
+            // Act 
+            try
+            {
+                _groupService.CreateGroup(group);
+            }
+            catch { }
+
+            // Assert 
+            var exception = Should.Throw<System.Data.DuplicateNameException>(() => _groupService.CreateGroup(group), "Group name already exists");
+        }
+
+        [Test]
+        public void CreateGroup_InvalidApplicationId_InvalidParameterException()
+        {
+            // Arrange 
+            var group = new Group()
+            {
+                Name = "Students",
+            };
+
+            _importingUnitOfWork.Setup(x => x.Groups)
+                .Returns(_groupRepository.Object);
+
+            _groupRepository.Setup(x => x.GetById(0))
+                .Returns(new Entities.Group());
+
+            _groupRepository.Setup(x => x.GetCount(It.IsAny<Expression<Func<Entities.Group, bool>>>()))
+                .Returns(0);
+
+            var fakeGuid = "FK8E36E2-D9A4-4F41-CD41-08D98C6D37B8";
+
+            var user = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
+            {
+                new Claim(ClaimTypes.Name, "example name"),
+                new Claim(ClaimTypes.NameIdentifier, fakeGuid),
+                new Claim("custom-claim", "example claim value"),
+            }, "mock"));
+
+            var context = new DefaultHttpContext();
+            context.User = user;
+
+
+            _httpContextAccessorMock.Setup(_ => _.HttpContext).Returns(context);
+
+            _httpContextAccessorMock.Setup(x => x.HttpContext.User)
+                .Returns(user);
+
+            // Act 
+            try
+            {
+                _groupService.CreateGroup(group);
+            }
+            catch { }
+
+            // Assert 
+            var exception = Should.Throw<InvalidParameterException>(() => _groupService.CreateGroup(group), "Something Went Wrong");
+        }
+
+        [Test]
+        public void GetGroup_GroupExists_ReturnGroupFromWorkerService()
+        {
+            // Arrange
+            var Id = 1;
 
             var group = new Group { Id = 1, Name = "Contacts" };
 
@@ -69,7 +216,7 @@ namespace DataImporter.Importing.Tests
             _groupRepository.Setup(x => x.GetById(Id))
                 .Returns(groupEntity);
 
-            _mapperMock.Setup(x => x.Map<Group>(groupEntity)).Returns(group);         
+            _mapperMock.Setup(x => x.Map<Group>(groupEntity)).Returns(group);
 
             // Act
             var result = _groupService.GetGroup(Id, true);
@@ -84,7 +231,7 @@ namespace DataImporter.Importing.Tests
         }
 
         [Test]
-        public void GetGroup_GroupExists_ReturnGroupFromWorkerService()
+        public void GetGroup_GroupExists_ReturnGroup()
         {
             // Arrange
             var Id = 1;
@@ -117,7 +264,7 @@ namespace DataImporter.Importing.Tests
             var context = new DefaultHttpContext();
             context.User = user;
 
-            
+
             _httpContextAccessorMock.Setup(_ => _.HttpContext).Returns(context);
 
             _httpContextAccessorMock.Setup(x => x.HttpContext.User)
@@ -132,6 +279,160 @@ namespace DataImporter.Importing.Tests
                 () => _groupRepository.VerifyAll(),
                 () => result.ShouldBe(group)
             );
+        }
+
+        [Test]
+        public void GetGroup_GroupNotExists_ReturnNullFromWorkerService()
+        {
+            // Arrange 
+            var Id = 0;
+            Group group = null;
+            Entities.Group groupEntity = null;
+
+            _importingUnitOfWork.Setup(x => x.Groups)
+                .Returns(_groupRepository.Object);
+
+            _groupRepository.Setup(x => x.GetById(Id))
+                .Returns(groupEntity);
+
+            _groupRepository.Setup(x => x.GetCount(It.IsAny<Expression<Func<Entities.Group, bool>>>()))
+                .Returns(1);
+
+            _mapperMock.Setup(x => x.Map<Group>(groupEntity)).Returns(group);
+
+
+            // Act 
+            var result = _groupService.GetGroup(Id, true);
+
+            //Assert
+            this.ShouldSatisfyAllConditions(
+                () => result.ShouldBe(group)
+                );
+        }
+
+        [Test]
+        public void GetGroup_GroupNotExists_ReturnNull()
+        {
+            // Arrange 
+            var Id = 0;
+            Group group = null;
+            Entities.Group groupEntity = null;
+
+            _importingUnitOfWork.Setup(x => x.Groups)
+                .Returns(_groupRepository.Object);
+
+            _groupRepository.Setup(x => x.GetById(Id))
+                .Returns(groupEntity);
+
+            _groupRepository.Setup(x => x.GetCount(It.IsAny<Expression<Func<Entities.Group, bool>>>()))
+                .Returns(1);
+
+            _mapperMock.Setup(x => x.Map<Group>(groupEntity)).Returns(group);
+
+            var fakeGuid = "FA8E36E2-D9A4-4F41-CD41-08D98C6D37B8";
+
+            var user = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
+            {
+                new Claim(ClaimTypes.Name, "example name"),
+                new Claim(ClaimTypes.NameIdentifier, fakeGuid),
+                new Claim("custom-claim", "example claim value"),
+            }, "mock"));
+
+            var context = new DefaultHttpContext();
+            context.User = user;
+
+
+            _httpContextAccessorMock.Setup(_ => _.HttpContext).Returns(context);
+
+            _httpContextAccessorMock.Setup(x => x.HttpContext.User)
+                .Returns(user);
+
+            // Act 
+            var result = _groupService.GetGroup(Id);
+
+            //Assert
+            this.ShouldSatisfyAllConditions(
+                () => result.ShouldBe(group)
+                );
+        }
+
+        [Test]
+        public void GetGroup_InvalidApplicationId_InvalidParameterException()
+        {
+            // Arrange
+
+            var fakeGuid = "foawejfgoa";
+
+            var user = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
+            {
+                            new Claim(ClaimTypes.Name, "example name"),
+                            new Claim(ClaimTypes.NameIdentifier, fakeGuid),
+                            new Claim("custom-claim", "example claim value"),
+            }, "mock"));
+
+            var context = new DefaultHttpContext();
+            context.User = user;
+
+            _httpContextAccessorMock.Setup(_ => _.HttpContext).Returns(context);
+
+            _httpContextAccessorMock.Setup(x => x.HttpContext.User)
+                .Returns(user);
+
+
+            // Act
+            try
+            {
+                var result = _groupService.GetGroup(0);
+
+            }
+            catch { }
+
+            // Assert
+            var exception = Should.Throw<InvalidParameterException>(() => _groupService.GetGroup(0), "Something Went Wrong");
+        }
+
+        [Test]
+        public void GetGroup_NotGroupOwner_InvalidParameterException()
+        {
+            // Arrange
+
+            _importingUnitOfWork.Setup(x => x.Groups)
+            .Returns(_groupRepository.Object);
+
+            _groupRepository.Setup(x => x.GetById(0))
+                .Returns(new Entities.Group());
+
+            _groupRepository.Setup(x => x.GetCount(It.IsAny<Expression<Func<Entities.Group, bool>>>()))
+                .Returns(0);
+
+            var fakeGuid = "FA8E36E2-D9A4-4F41-CD41-08D98C6D37B8";
+
+            var user = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
+            {
+                            new Claim(ClaimTypes.Name, "example name"),
+                            new Claim(ClaimTypes.NameIdentifier, fakeGuid),
+                            new Claim("custom-claim", "example claim value"),
+            }, "mock"));
+
+            var context = new DefaultHttpContext();
+            context.User = user;
+
+            _httpContextAccessorMock.Setup(_ => _.HttpContext).Returns(context);
+
+            _httpContextAccessorMock.Setup(x => x.HttpContext.User)
+                .Returns(user);
+
+
+            // Act
+            try
+            {
+                var result = _groupService.GetGroup(0);
+
+            }
+            catch { }
+
+            // Assert
+            var exception = Should.Throw<InvalidParameterException>(() => _groupService.GetGroup(0), "Unauthorized Access");
         }
     }
 }
